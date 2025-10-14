950a20c8a43632be4f2c9a7cca0be29a53c94dc8
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-10-14 09:16:40 +0200

    Revert "feat(mobile): add essential setup for flutter project"
    This reverts commit 5cb16e0117b40b4a36a38ee31734b0aa2bec9ddc.

--- mobile/lib/core/di/di.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart' as http;
-
-import '../../data/repositories/game_repository_impl.dart';
-import '../../domain/repositories/game_repository.dart';
-
-class DI {
-  static GameRepository provideGameRepository() => GameRepositoryImpl(client: http.Client());
-}
